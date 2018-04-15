19a2673899f9b8752d9afe45cacf7b231379f0d8
NaderNasr <nadernasr7@gmail.com>
2018-04-15 14:30:59 +0300

    UI Polish

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -67,11 +67,11 @@ class App extends React.Component {
 
 
     return (<div>
-      <h1 style ={{color:'black', fontSize: 100}}> Gorcery List </h1>
-      <input style={{width: 500}} name = "test" onChange = { this.getTodo } />
-      <button onClick = {() => this.addTodo( this.state.todo )}> Add to ToDo List</button>
+      <h1 style ={{color:'black', fontSize: 100, textAlign: 'center'}}> Gorcery List </h1>
+      <input style={{width: 350, align:'center'}} name = "test" onChange = { this.getTodo } />
+      <button style ={{width: 350}} onClick = {() => this.addTodo( this.state.todo )}> Add to ToDo List</button>
 {/*   <button onClick = {() => this.deleteTodo( this.state.todo )}> Delete an item </button>*/}
-      <List items = { this.state.items }/>
+      <List items = { this.state.items } />
 
     </div>)
   }
