ae38b4cff1c64964062a39ff554af7779310a739
NaderNasr <nadernasr7@gmail.com>
2018-04-15 14:41:16 +0300

    UI Polish Alignment

--- react-client/src/index.jsx
+++ react-client/src/index.jsx
@@ -67,7 +67,7 @@ class App extends React.Component {
 
 
     return (<div>
-      <h1 style ={{color:'black', fontSize: 100, textAlign: 'center'}}> Gorcery List </h1>
+      <h1 style ={{color:'black', fontSize: 100, textAlign: 'center'}}>Gorcery List </h1>
       <input style={{width: 350, align:'center'}} name = "test" onChange = { this.getTodo } />
       <button style ={{width: 350}} onClick = {() => this.addTodo( this.state.todo )}> Add to ToDo List</button>
 {/*   <button onClick = {() => this.deleteTodo( this.state.todo )}> Delete an item </button>*/}
